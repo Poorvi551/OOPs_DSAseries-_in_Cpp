bcc3dc2efe1a36efce45addaec10187a917f6ea0
S B Poorvi <182614650+Poorvi551@users.noreply.github.com>
2026-04-13 19:38:59 +0530

    Add files via upload

---
+++ destructor.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student{
+    public:
+    string name;
+    double* cgpaPtr;
+
+    Student(string name, double cgpa){
+        this->name = name;
+        cgpaPtr= new double;
+        *cgpaPtr=cgpa;
+    }
+    // Destructor to free the dynamically allocated memory
+    ~Student(){
+        cout<< "Hi, I deletes everything"<< endl;
+    }
+    void getInfo(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"CGPA: "<<*cgpaPtr<<endl;
+    }
+};
+
+int main(){
+    Student s1("Shradha", 8.9);
+    s1.getInfo();
+    return 0;
+}
